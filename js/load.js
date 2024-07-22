6406cee1dd22b983e68ff7bbf4b789e5e5f39ef4
jackestar <juachojesusgr@gmail.com>
2024-07-22 12:15:30 -0400

    Other reset...

--- js/load.js
+++
@@ -1,9 +0,0 @@
-tog = (e) => {
-	e.parentElement.classList.add('ready')
-	setTimeout(()=>{
-		document.querySelector('section.login').classList.add('ready')
-	},1500)
-		setTimeout(()=>{
-		document.querySelector('section.login').classList.add('ready2')
-	},2000)
-}
