c632abdb9dc0cf71634aa6d03be49209364dc592
jackestar <juanchojesusgr@gmail.com>
2026-01-01 21:28:24 -0400

    Nexlify ln handler

--- postbuild.sh
+++ postbuild.sh
@@ -13,6 +13,5 @@ curl -L -o $models/TDA75610.glb "https://drive.google.com/uc?export=download&id=
 curl -L -o $models/ESP01-Driver.glb "https://drive.google.com/uc?export=download&id=1sb9Hfd1ILrwnXJWMcObeMpFSFNX6RJ4Q"
 curl -L -o $models/UltraSonic.glb "https://drive.google.com/uc?export=download&id=19QxXfrL7RRCJn_lSFvxieGpd3TcvdyOa"
 
-ln -s $models ./dist/models
 # Translations
 # node translate.js --input ./Locale/ --output ./dist --filenameLang
